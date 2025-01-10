5d0d843814aac2a78722e6de8c054cf0491f4690
alexxware <manuelmorm1@gmail.com>
2025-01-09 21:23:14 -0600

    [+] creacion de registro screen

--- app/src/main/java/com/example/agendaonline/MainActivity.kt
+++ app/src/main/java/com/example/agendaonline/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.agendaonline
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnIngresar: Button
+    lateinit var btnRegistrarse: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnIngresar = findViewById(R.id.btnIngresar)
+        btnRegistrarse = findViewById(R.id.btnRegistrarse)
+
+        btnIngresar.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+        btnRegistrarse.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
         }
     }
 }
